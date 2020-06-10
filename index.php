24d5c3e59ba83e66c5e94e79494eb579eac342cf
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 16:34:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    if ( isset($_POST['login']) && isset($_POST['password'])){
+        setcookie('login', $_POST['login'], time() + 365*24*3600);
+        setcookie('password', $_POST['password'], time() + 365*24*3600);
+    }
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Partie 8 exercice 3</title>
+    <style>
+        form {
+
+            display: flex;
+            flex-direction: column;
+        }
+        label {
+            margin: 0 auto;
+            margin-top: 10px;
+        }
+        #sendBtn {
+            width: 80px;
+            margin: 0 auto;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <label for="login">Login : <input type="text" name="login" id="login"></label>
+        <label for="password">Password : <input type="password" name="password" id="password"></label>
+        <input type="submit" value="Envoyer" id="sendBtn">
+    </form>
+</body>
+</html>
